8524f904de294f9c1037dd86c1f29319837c019f
PabloGelvez <gelvezpablo12@gmail.com>
2024-10-31 20:48:42 -0500

    chore: update folder Scripts

---
+++ Scripts/Scripts_auto/load_create_tables.bat
@@ -0,0 +1,23 @@
+@echo off
+chcp 65001
+setlocal
+
+:: Configura las variables de conexión
+set "PGHOST=localhost"
+set "PGPORT=5432"
+set "PGUSER=adminzoo"
+set "PGPASSWORD=Pablogelvez1912."
+set "PGDATABASE=zoologico"
+
+:: Ejecuta cada archivo SQL en orden
+
+echo Ejecutando TableDefinitions.sql...
+psql -h %PGHOST% -p %PGPORT% -U %PGUSER% -d %PGDATABASE% -f "C:\Users\usuario\OneDrive\Escritorio\Git\GitHub\Doc-UP-PabloGelvez\BDl_GB-ZOO\Scripts\ddl\create_tables.sql"
+if %errorlevel% neq 0 (
+    echo Error ejecutando TableDefinitions.sql
+    exit /b %errorlevel%
+)
+
+echo Script ejecutado correctamente.
+endlocal
+pause
